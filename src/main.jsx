bea8bab05fc0d3c3022622f39c1c476b6168e90a
Moch Farrel <91719525+mochfarrel06@users.noreply.github.com>
2024-01-09 11:41:23 +0700

    delete style css

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import store from './app/store.js'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import store from "./app/store.js";
+import {Provider} from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
